cad7eba184d966585d34af5188ea74f7a62be32e
Aaron Van Canneyt <aaron@scalefactory.eu>
2024-02-15 13:09:58 +0100

    fixes

--- dist/main.js
+++ dist/main.js
@@ -90,51 +90,45 @@ function debounce (func, wait, immediate) {
 }
 
 function paintSettings () {
-	document.getElementById('text-input-id-0').value = mapsKey;
-	document.getElementById('text-input-id-1').value = address;
-	document.getElementById('slider-id-01').value = width;
-	document.getElementById('slider-id-02').value = height;
-	document.getElementById('slider-id-03').value = zoom;
+	document.getElementById('title-id').value = title;
+	document.getElementById('image-width-id').value = width;
+	document.getElementById('image-height-id').value = height;
+	document.getElementById('zoom-level-id').value = zoom;
 }
 
 function paintSliderValues () {
-	document.getElementById('slider-id-01-val').innerHTML = document.getElementById('slider-id-01').value;
-	document.getElementById('slider-id-02-val').innerHTML = document.getElementById('slider-id-02').value;
-	document.getElementById('slider-id-03-val').innerHTML = document.getElementById('slider-id-03').value;
+	document.getElementById('image-width-id-val').innerHTML = document.getElementById('image-width-id').value;
+	document.getElementById('image-height-id-val').innerHTML = document.getElementById('image-height-id').value;
+	document.getElementById('zoom-level-id-val').innerHTML = document.getElementById('zoom-level-id').value;
 }
 
 function paintMap() {
-	mapsKey = document.getElementById('text-input-id-0').value;
-	address = document.getElementById('text-input-id-1').value;
-	width = document.getElementById('slider-id-01').value;
-	height = document.getElementById('slider-id-02').value;
-	zoom = document.getElementById('slider-id-03').value;
-	link = document.getElementById('text-input-id-2').value;
-	if (!address) {
-		return;
-	}
-	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
-		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
-		'&markers=' + address.split(' ').join('+') + '&key=' + mapsKey;
-	sdk.setContent('<a href="' + link + '"><img src="' + url + '" /></a>');
+	console.log('painting map');
+	title = document.getElementById('title-id').value;
+	width = document.getElementById('image-width-id').value;
+	height = document.getElementById('image-height-id').value;
+	zoom = document.getElementById('zoom-level-id').value;
+
+	sdk.setContent(
+		'<h1>'+title+'</h1>' +
+		'<p>h1 will be a dynamic component, when you change the title on the lift it will apear on the right</p>'+
+		'<p>'+width+'/<p>' +
+		'<p>'+height+'/<p>' +
+		'<p>'+zoom+'/<p>'
+	);
 	sdk.setData({
-		address: address,
+		title: address,
 		width: width,
 		height: height,
-		zoom: zoom,
-		link: link,
-		mapsKey: mapsKey
+		zoom: zoom
 	});
-	localStorage.setItem('googlemapsapikeyforblock', mapsKey);
 }
 
 sdk.getData(function (data) {
-	address = data.address || '';
+	title = data.title || '';
 	width = data.width || 400;
 	height = data.height || 300;
 	zoom = data.zoom || 15;
-	link = data.link || '';
-	mapsKey = data.mapsKey || localStorage.getItem('googlemapsapikeyforblock');
 	paintSettings();
 	paintSliderValues();
 	paintMap();
